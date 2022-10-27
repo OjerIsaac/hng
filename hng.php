1ff4acebf72149c93d5be1c910916f7e7231ef65
Ojer Isaac <isaacojerumu@gmail.com>
2022-10-27 08:30:42 +0100

    endpoint

---
+++ hng.php
@@ -0,0 +1,15 @@
+<?php
+
+$input = @file_get_contents("php://input");
+$data = json_decode($input, true);
+
+if (empty($data))
+{
+    // $returnArr = array("errorCode" => "401", "status" => "false", "ResponseMsg" => "Something Went Wrong!");
+    $returnArr = array("slackUsername" => "ejiro", "backend" => true, "age" => 22, "bio" => "Backend developer @ HNG");
+} else
+{
+    // $returnArr = array();
+}
+
+echo json_encode($returnArr);
